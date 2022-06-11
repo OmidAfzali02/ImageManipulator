42f0b71048ca240dc4573d7a586280b7f2e94627
OmidAfzali02 <dev.omid02@gmail.com>
2022-06-12 00:12:20 +0430

    Stegano encoding ability added

--- Manipulator.py
+++ Manipulator.py
@@ -7,22 +7,40 @@ modes = ["0", "1", "2", "3", "4", "5", "q"]
 
 while True:
     def main():
-        print("Welcome to Image Manipulator By Omid Afzali")
+        print("Welcome to Image Manipulator By Omid Afzali\n")
         print("0- Stegano graph encoding")
         print("1- Stegano graph decoding")
+        print("2- ascii creator")
         print("q- Quit")
 
-        mode = input("Select preferred mode:  ")
+        mode = input("\nSelect preferred mode:  ")
 
         if mode not in modes:
-            print("Select an available mode!! ")
+            print("\nSelect an available mode!! ")
         else:
             return mode.lower()
 
 
+    def stegano_graph_hide(img, msg):
+        img = img
+        res = 'stegano art.png'
+        msg = msg
+
+        try:
+            lsb.hide(img, msg).save(res)
+            print("done")
+        except:
+            print("Error occurred")
+
     mode = main()
     if mode == "0":
-        pass
+        print("Stegano graph encode mode loaded")
+        print("Select Image file:  ")
+        img = tkinter.filedialog.askopenfilename()
+        msg = input("Now enter your message:  ")
+        stegano_graph_hide(img, msg)
+        print(f"All done!\n Your stegano art is in the same folder as the image\n")
+
     elif mode == "1":
         pass
     elif mode == "2":
